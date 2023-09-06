271e8dd268097076671bb595efaba033123598b3
Rhina Kim <chihiroanihr@gmail.com>
2023-09-05 22:43:05 -0400

    API Code (SSG/SSR/ISR)

--- nextjs-13-catchup-tutorial/app/todos/page.tsx
+++ nextjs-13-catchup-tutorial/app/todos/page.tsx
@@ -4,6 +4,15 @@ import styles from "../page.module.css";
 
 async function getData() {
   const res = await fetch("http://localhost:3000/api/todos");
+  /*
+  const res = await fetch("http://localhost:3000/api/todos", {API_CODE});
+
+  API_CODE:
+  - {cache: "no-cache"} --> SSG (静的データ取得)
+  - {cache: "no-store"} --> SSR (動的データ取得)
+  - {next: {revalidate: 10}} --> ISR (データの再検証) after 10s.
+  */
+
   return res.json();
 }
 
